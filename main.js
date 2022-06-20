4b78d2015e90fd3f0d0aa1a9b0dcf353ec0c9f9d
Hugo Perez <hugopereztm12309@gmail.com>
2022-06-20 11:17:07 -0400

    Have the computer return rock, paper, or scissors
    The computer(opponent) can now choose between rock, paper, and scissors.
    It will return either one of those 3 options as a string.

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
+const Rock = "Rock";
+const Paper = "Paper";
+const Scissors = "Scissors";
+
 computerPlay = () => {
     let play = randomInteger(1,3); // Between 1 - 3
-
+    switch(play) {
+        case 1 : 
+            return Rock;
+        case 2: 
+            return Paper;
+        case 3: 
+            return Scissors;
+    }
 }
 randomInteger = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
